d717d147cbd68763610006c4f7c5ea57bd1f88b2
Jacob Bowlware <jacobbowlware@gmail.com>
2023-10-18 22:24:22 -0500

    Include bootstrap.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,42 @@
+
 const Home = () => {
     return (
-        <div></div>
+        <div className="web-page">
+            <div className="web-section home">
+                <div className="web-section__container home__container">
+                    <div className="home__container-item">
+                        <h1 className="home__container-item__header">
+                            SurfSight
+                        </h1>
+                        <h2 className="home__container-item__subheader">
+                            Track your online surfing
+                        </h2>
+                        <h2 className="home__container-item__subheader">
+                            Notice your bad habits
+                        </h2>
+                        <h2 className="home__container-item__subheader">
+                            Improve your productivity
+                        </h2>
+                        <button className="btn">
+                            Subscribe
+                        </button>
+                    </div>
+                    <div className="home__container-item">
+
+                    </div>
+                </div>
+            </div>
+            <div className="web-section">
+                <div className="web-section__container home__container">
+                    <div className="home__container-item">
+                        What is life
+                    </div>
+                    <div className="home__container-item">
+                        asdf
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
