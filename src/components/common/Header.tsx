3c780525627703a8736592c37c845dfdab827d43
Jacob Bowlware <jacobbowlware@gmail.com>
2023-10-22 13:59:29 -0500

    Implement basic navbar and home hero.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,18 +1,18 @@
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand" href="#">Cultivated</a>
+        <nav className="web-section navbar navbar-expand-lg navbar-light">
+            <a className="navbar-brand" href="#">SurfSight</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="/#">Features</a>
+                        <a className="nav-link" href="/#">Login</a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/#">Pricing</a>
+                    <li className="nav-item nav-item__highlight">
+                        <a className="nav-link" href="/#">Get SurfSight</a>
                     </li>
                 </ul>
             </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,34 +5,15 @@ const Home = () => {
             <div className="web-section home">
                 <div className="web-section__container home__container">
                     <div className="home__container-item">
-                        <h1 className="home__container-item__header">
-                            SurfSight
-                        </h1>
-                        <h2 className="home__container-item__subheader">
-                            Track your online surfing
+                        <h2 className="home__container-item__header">
+                            Track your online activity
                         </h2>
-                        <h2 className="home__container-item__subheader">
-                            Notice your bad habits
-                        </h2>
-                        <h2 className="home__container-item__subheader">
-                            Improve your productivity
-                        </h2>
-                        <button className="btn">
-                            Subscribe
+                        <p className="home__container-item__p">
+                            The chrome extension that tracks your online surfing and gives you a report of your online activity.
+                        </p>
+                        <button className="btn home__container-item__btn">
+                            Start tracking now --&gt;
                         </button>
-                    </div>
-                    <div className="home__container-item">
-
-                    </div>
-                </div>
-            </div>
-            <div className="web-section">
-                <div className="web-section__container home__container">
-                    <div className="home__container-item">
-                        What is life
-                    </div>
-                    <div className="home__container-item">
-                        asdf
                     </div>
                 </div>
             </div>
