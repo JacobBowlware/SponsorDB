5d67eb4e431f52ee5c57298c83eccd2305582540
Jacob Bowlware <jacobbowlware@gmail.com>
2023-10-31 20:58:20 -0500

    Improve FeatureCard styling.

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -7,15 +7,17 @@ interface FeatureCardProps {
     highlighted?: boolean;
 }
 
+// Components CSS: src/componnets/css/Home.css
 const FeatureCard = ({ icon, header, text, highlighted }: FeatureCardProps) => {
-
     if (highlighted) {
         return (
             <div className="home__features-list__item home__features-list__item--highlighted">
-                <FontAwesomeIcon className="home__features-list__item-icon home__features-list__item-icon--highlighted" icon={icon} />
-                <h3 className="home__features-list__item-header">
-                    {header}
-                </h3>
+                <div className="home__features-list__item-header-container">
+                    <FontAwesomeIcon className="home__features-list__item-icon home__features-list__item-icon--highlighted" icon={icon} />
+                    <h3 className="home__features-list__item-header">
+                        {header}
+                    </h3>
+                </div>
                 <p className="home__features-list__item-text">
                     {text}
                 </p>
@@ -25,10 +27,12 @@ const FeatureCard = ({ icon, header, text, highlighted }: FeatureCardProps) => {
 
     return (
         <div className="home__features-list__item">
-            <FontAwesomeIcon className="home__features-list__item-icon" icon={icon} />
-            <h3 className="home__features-list__item-header">
-                {header}
-            </h3>
+            <div className="home__features-list__item-header-container">
+                <FontAwesomeIcon className="home__features-list__item-icon" icon={icon} />
+                <h3 className="home__features-list__item-header">
+                    {header}
+                </h3>
+            </div>
             <p className="home__features-list__item-text">
                 {text}
             </p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 // Font Awesome Icons
-import { faArrowRight, faBell, faClipboard, faClock, faSliders } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faBook, faMoneyBill, faRocket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FeatureCard from "../components/FeatureCard";
 
@@ -17,10 +17,10 @@ const Home = () => {
                 <div className="web-section__container home__container">
                     <div className="home__container-item">
                         <h2 className="home__container-item__header">
-                            Maximize Your Podcast Sponsorships
+                            Maximize Your Podcast's Sponsors
                         </h2>
                         <p className="home__container-item__p">
-                            Let SponsorTrail scower the web for you, finding the best sponsorships for your podcast.
+                            Let SponsorTrail do the legwork for you, uncovering top-tier sponsorships to boost your podcast and revenue potential.
                         </p>
                         <button className="btn home__container-item__btn">
                             Learn More
@@ -30,14 +30,14 @@ const Home = () => {
                 </div>
             </div>
             <div className="web-section" id="features">
-                <div className="web-section__container">
+                <div className="web-section__container web-section-content">
                     <h2 className="web-section__container-header">
                         Features
                     </h2>
                     <div className="home__features-list">
-                        <FeatureCard icon={faClock} header="Activity Tracking" text="Real-time monitoring of your online actions." />
-                        <FeatureCard icon={faBell} header="Alerts and Goals" text="Set goals and receive alerts to manage screen time effectively." />
-                        <FeatureCard highlighted={true} icon={faClipboard} header="Usage Reports" text="Detailed reports on browsing habits, showing most-visited sites and daily usage." />
+                        <FeatureCard icon={faBook} header="Weekly Sponsorship Updates" text="Receive a curated list of podcast sponsors every week and stay informed with." />
+                        <FeatureCard icon={faRocket} header="Never Miss Out" text="Get notified when new sponsorship opportunities are available, so you can be the first to apply." />
+                        <FeatureCard highlighted={true} icon={faMoneyBill} header="Maximize Sponsorships" text="Discover a wide range of sponsorship opportunities, boosting your podcast's revenue potential." />
                     </div>
                 </div>
             </div>
