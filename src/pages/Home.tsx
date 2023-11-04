6a781fbbd52014e1c6c2d256da1871c8fa1be078
Jacob Bowlware <jacobbowlware@gmail.com>
2023-11-04 13:00:24 -0500

    Implement Home::Testimonials section & TestimonialCard component.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@
 import { faArrowRight, faBook, faMoneyBill, faRocket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FeatureCard from "../components/FeatureCard";
+import TestimonialCard from "../components/TestimonialCard";
 
 
 /**
@@ -32,12 +33,24 @@ const Home = () => {
             <div className="web-section" id="features">
                 <div className="web-section__container web-section-content">
                     <h2 className="web-section__container-header">
-                        Features
+                        Why SponsorTrail?
                     </h2>
                     <div className="home__features-list">
                         <FeatureCard icon={faBook} header="Weekly Sponsorship Updates" text="Receive a curated list of podcast sponsors every week and stay informed with." />
                         <FeatureCard icon={faRocket} header="Never Miss Out" text="Get notified when new sponsorship opportunities are available, so you can be the first to apply." />
                         <FeatureCard highlighted={true} icon={faMoneyBill} header="Maximize Sponsorships" text="Discover a wide range of sponsorship opportunities, boosting your podcast's revenue potential." />
+                    </div>
+                </div>
+            </div>
+            {/* */}
+            <div className="web-section" id="testimonials">
+                <div className="web-section__container web-section-content">
+                    <h2 className="web-section__container-header-sm">
+                        What People Are Saying
+                    </h2>
+                    <div className="home__testimonials-list">
+                        <TestimonialCard name="John Doe" affiliation="Podcast Host" quote="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem recusandae commodi, neque laudantium soluta nihil quae enim expedita odit aliquam." />
+                        <TestimonialCard name="Demarcus Oslow" affiliation="Podcast Sponsor" quote="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem recusandae commodi, neque laudantium soluta nihil quae enim expedita odit aliquam." />
                     </div>
                 </div>
             </div>
