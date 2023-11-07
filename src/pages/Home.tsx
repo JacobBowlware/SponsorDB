91cb4c3abac7680c864edc7cbf787d9621aa3559
Jacob Bowlware <jacobbowlware@gmail.com>
2023-11-07 11:24:18 -0600

    Implement basic Home::How-It-Works section.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FeatureCard from "../components/FeatureCard";
 import TestimonialCard from "../components/TestimonialCard";
 
+// Images
+import search from './../assets/images/search.png';
+import list from './../assets/images/list.png';
 
 /**
  * TODO:
  * - Add "Features" section. - DONE
+ * - Add "Testimonials" section - DONE
  * - Add "How it works" section.
- * - Add "Testimonials" section.
+ * - Add "Pricing" section.
+ * - Add "FAQ" section.
+ * - Add "Email 
  */
 const Home = () => {
     return (
@@ -42,7 +48,6 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/* */}
             <div className="web-section" id="testimonials">
                 <div className="web-section__container web-section-content">
                     <h2 className="web-section__container-header-sm">
@@ -51,6 +56,42 @@ const Home = () => {
                     <div className="home__testimonials-list">
                         <TestimonialCard name="John Doe" affiliation="Podcast Host" quote="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem recusandae commodi, neque laudantium soluta nihil quae enim expedita odit aliquam." />
                         <TestimonialCard name="Demarcus Oslow" affiliation="Podcast Sponsor" quote="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem recusandae commodi, neque laudantium soluta nihil quae enim expedita odit aliquam." />
+                    </div>
+                </div>
+            </div>
+            <div className="web-section" id="how-it-works">
+                <div className="web-section__container web-section-content">
+                    <h2 className="web-section__container-header-sm">
+                        How It Works
+                    </h2>
+                    <div className="home__how-it-works-container">
+                        <div className="home__how-it-works-container__item">
+                            <h3 className="home__how-it-works-container__item-header">
+                                Step 1: We Gather Data
+                            </h3>
+                            <p className="home__how-it-works-container__item-text">
+                                Our custom software will scower the web and gather important data on podcast sponsors.
+                            </p>
+                        </div>
+                        <img className="home__how-it-works-container__img" src={search} alt="A team searching the web and gathering data on potential podcast sponsors for our clients." />
+                        <img className="home__how-it-works-container__img" src={list} alt="A team preparing to send a list of potential podcast sponsors to our clients." />
+                        <div className="home__how-it-works-container__item">
+                            <h3 className="home__how-it-works-container__item-header">
+                                Step 2: We Send You a List
+                            </h3>
+                            <p className="home__how-it-works-container__item-text">
+                                After we've gathered the data, we'll send you a list of potential sponsors. You will recieve a new list every week, so you never miss out on a sponsorship opportunity.
+                            </p>
+                        </div>
+                        <div className="home__how-it-works-container__item">
+                            <h3 className="home__how-it-works-container__item-header">
+                                Step 3: You Apply
+                            </h3>
+                            <p className="home__how-it-works-container__item-text">
+                                With the list of potential sponsors, you can apply to the ones you're interested in. Waste no time searching for sponsors, and focus on what matters most: your podcast.
+                            </p>
+                        </div>
+                        <img className="home__how-it-works-container__img" src={list} alt="Person applying themselves and applying for Podcast sponsorships." />
                     </div>
                 </div>
             </div>
