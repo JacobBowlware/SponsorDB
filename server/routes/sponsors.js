c90e744d0ff861c4e602a6bf68b36e3d5531f005
Jacob Bowlware <jacobbowlware@gmail.com>
2024-03-19 11:18:50 -0500

    Refactor & simplify testing

--- server/routes/sponsors.js
+++ server/routes/sponsors.js
@@ -7,37 +7,12 @@ const subscribe = require('../middleware/subscribe');
 const admin = require('../middleware/admin');
 
 // Get all sponsors
-router.get('/', async (req, res) => {
+router.get('/', auth, subscribe, async (req, res) => {
     const sponsors = await Sponsor.find();
     res.status(200).send(sponsors);
 });
 
-// Create a new sponsor
-router.post('/', auth, async (req, res) => {
-    const { error } = validateSponsor(res.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-
-    const sponsor = new Sponsor({
-        sponsorName: req.body.sponsorName?.toLowerCase(),
-        sponsorLink: req.body.sponsorLink,
-    });
-
-    // Check if sponsor already exists
-    const sponsorExists = await Sponsor.findOne({ sponsorName: req.body.sponsorName?.toLowerCase() });
-    if (sponsorExists) {
-        return res.status(400).send('Sponsor already exists');
-    }
-
-    await sponsor.save().then((sponsor) => {
-        res.send(sponsor);
-    }).catch((e) => {
-        console.log(e);
-        res.status(400).send(e.message);
-    })
-})
-
+// Get the number of sponsors and sponsorships
 router.get('/count', async (req, res) => {
     const sponsors = await Sponsor.find();
     const sponsorships = await Sponsorship.find();
@@ -45,6 +20,35 @@ router.get('/count', async (req, res) => {
     const sponsorLength = sponsors.length;
     const sponsorshipLength = sponsorships.length;
     res.status(200).send({ "sponsorshipLength": sponsorshipLength, "sponsorLength": sponsorLength });
+});
+
+// Create a new sponsor
+router.post('/', auth, async (req, res) => {
+    const { error } = validateSponsor(res.body);
+    if (error) {
+        if (error.details[0].message.includes('sponsorName')) {
+            return res.status(400).send('Invalid sponsor name');
+        }
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const sponsor = new Sponsor({
+        sponsorName: req.body.sponsorName.toLowerCase(),
+        sponsorLink: req.body.sponsorLink,
+        tags: req.body.tags
+    });
+
+    // Check if sponsor already exists
+    const sponsorExists = await Sponsor.findOne({ sponsorName: req.body.sponsorName.toLowerCase() });
+    if (sponsorExists) {
+        return res.status(400).send('Sponsor already exists');
+    }
+
+    await sponsor.save().then((sponsor) => {
+        res.send(sponsor);
+    }).catch((e) => {
+        res.status(400).send(e.message);
+    })
 });
 
 module.exports = router;
--- server/tests/integration/routes/sponsors.test.js
+++ server/tests/integration/routes/sponsors.test.js
@@ -2,9 +2,13 @@ const app = require('../../../index');
 const request = require('supertest');
 const { Sponsor } = require('../../../models/sponsor');
 const { Sponsorship } = require('../../../models/sponsorship');
-
+const { User } = require('../../../models/user');
+const auth = require('../../../middleware/auth');
+const mongoose = require('mongoose');
 
 describe('/api/sponsors', () => {
+    // Define the happy path, and then in each test, we change
+    // one parameter that clearly aligns with the name of the test.
     afterEach(async () => {
         await Sponsor.deleteMany();
         await Sponsorship.deleteMany();
@@ -25,10 +29,32 @@ describe('/api/sponsors', () => {
                 }
             ])
 
-            const res = await request(app).get('/api/sponsors/');
+            const user = new User({
+                email: "testing@gmail.com",
+                password: "1234",
+                isSubscribed: true
+            })
+
+            const token = user.generateAuthToken();
+
+            const res = await request(app).get('/api/sponsors/').set("x-auth-token", token);
 
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(2);
+        });
+
+        it('should return 401 if user is not logged in', async () => {
+            const res = await request(app).get('/api/sponsors/');
+
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 403 if user is not subscribed', async () => {
+            const token = new User().generateAuthToken();
+
+            const res = await request(app).get('/api/sponsors/').set("x-auth-token", token);
+
+            expect(res.status).toBe(403);
         })
     })
 
@@ -73,6 +99,30 @@ describe('/api/sponsors', () => {
             expect(res.body.sponsorshipLength).toBe(2);
             expect(res.body.sponsorLength).toBe(3);
             expect(res.status).toBe(200);
-        })
+        });
+    })
+
+    describe('POST /', () => {
+        it('should return 401 if user is not logged in', async () => {
+            const res = await request(app).post('/api/sponsors/').send({ sponsorName: "Sponsor1", sponsorLink: "http://www.test-sponsor-1.com", tags: ["tag1", "tag2"] });
+
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 400 if sponsorName is invalid', async () => {
+            const token = new User().generateAuthToken();
+            const req = { header: jest.fn() };
+            const resF = {
+                status: jest.fn().mockReturnValue({
+                    send: jest.fn()
+                })
+            };
+            const next = jest.fn();
+
+            auth(req, resF, next);
+            const res = await request(app).post('/api/sponsors/').set("x-auth-token", token).send({ sponsorName: "", sponsorLink: "http://www.test-sponsor-1.com", tags: ["tag1", "tag2"] });
+
+            expect(res.status).toBe(400);
+        });
     })
 })
